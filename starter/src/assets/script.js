400fee965b9abd1c59c1c8b9674bd15fac700207
dontia <dontia@users.noreply.github.com>
2024-09-14 11:42:29 -0400

    style: Remove unnecessary comments and test-related code

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -154,26 +154,3 @@ function pay(amount) {
     return amount;
   }
 }
-
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
-
-
-/* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
-*/
-
-module.exports = {
-  products,
-  cart,
-  addProductToCart,
-  increaseQuantity,
-  decreaseQuantity,
-  removeProductFromCart,
-  cartTotal,
-  pay,
-  emptyCart,
-  /* Uncomment the following line if completing the currency converter bonus */
-  // currency
-}
