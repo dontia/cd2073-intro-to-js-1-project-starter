725c0e10bac86d8ee133750b90438a49753d5577
dontia <dontia@users.noreply.github.com>
2024-09-15 21:21:33 -0400

    test: Reinsert unit test configuration and exports
    * Reinsert the section of code for running unit tests
    * Include the `module.exports` object to export functions and variables for testing

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -189,3 +189,24 @@ function pay(amount) {
   // Return the remaining amount
   return remaining;
 }
+
+
+/* The following is for running unit tests. 
+   To fully complete this project, it is expected that all tests pass.
+   Run the following command in terminal to run tests
+   npm run test
+*/
+
+module.exports = {
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  // currency
+}
